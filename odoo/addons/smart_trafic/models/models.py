33ca75a8b00c925f431e8409331b1c1259c309de
Abiezer <jsifontes@openti.cl>
2019-05-28 19:01:33 -0400

    add congested road

--- odoo/addons/smart_trafic/models/models.py
+++ odoo/addons/smart_trafic/models/models.py
@@ -2,9 +2,9 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 from odoo import models, fields, api
 
-# class smart_trafic(models.Model):
-#     _name = 'smart_trafic.smart_trafic'
-
+class smart_trafic(models.Model):
+    _name = 'smart_trafic.smart_trafic'
+    _auto = False
 #     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
---
+++ odoo/addons/smart_trafic/static/src/js/map_controller.js
@@ -0,0 +1,6 @@
+/*
+odoo.define('smart_trafic',function(require){
+    "use strict";
+    var Widget = require('web.Widget');
+});
+*/
