4075d985c475e3aa1a4bb07e30fee174ee386f5a
Abiezer <jsifontes@openti.cl>
2019-06-07 10:58:40 -0400

    wait that of dom charge

--- odoo/addons/smart_trafic/static/src/js/smart_trafic_controller.js
+++ odoo/addons/smart_trafic/static/src/js/smart_trafic_controller.js
@@ -1,6 +1,7 @@
 odoo.define('Map.Controller',function(require){
   'use strict';
   var AbstractController = require('web.AbstractController');
+  var MapRenderer = require('Map.Renderer');
   var core = require('web.core');
   var qweb = core.qweb;
 
@@ -11,9 +12,9 @@ odoo.define('Map.Controller',function(require){
                this.$buttons = $(qweb.render('ViewSmartTrafic.buttons'));
                this.$buttons.appendTo($node);
                this.$buttons.on('click', 'button#add', this._onAddButtonClick.bind(this));
-               this.$buttons.on('click', 'button#map1', this._onMapButtonClick.bind(this));
-               this.$buttons.on('click', 'button#map2', this._onMapButtonClick.bind(this));
-               this.$buttons.on('click', 'button#map3', this._onMapButtonClick.bind(this));
+               // this.$buttons.on('click', 'button#map1', this._onMapButtonClick.bind(this));
+               // this.$buttons.on('click', 'button#map2', this._onMapButtonClick.bind(this));
+               // this.$buttons.on('click', 'button#map3', this._onMapButtonClick.bind(this));
            }
 
        },
@@ -29,12 +30,7 @@ odoo.define('Map.Controller',function(require){
 
        },
        _onMapButtonClick: function(ev){
-        this.$el.find(".o_view_manager_content").empty();
-        this.$el.find(".o_view_manager_content").html('<div><div><div class="row ml16 mr16"><div id="mapId" class="map map-home" style="margin:12px 0 12px 0;height:500px;"></div></div></div></div>');
-        var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
-        var map = L.map('mapId').setView([23.140445, -82.351644], 17).addLayer(osm);
+         MapRenderer._render();
        }
   });
 
--- odoo/addons/smart_trafic/static/src/js/smart_trafic_rendered.js
+++ odoo/addons/smart_trafic/static/src/js/smart_trafic_rendered.js
@@ -11,20 +11,47 @@ odoo.define('Map.Renderer',function(require){
             'click .o_primay_button': '_onClickButton',
         }),
         start: function(){
-          // alert("startRendered");
           return this._super.apply(this, arguments);
+
         },
         willStart: function(){
           // alert("willStartRendered");
           return this._super.apply(this, arguments);
         },
+        /**
+         * The graph view uses the Leaflet render the map. This lib requires
+         * that the rendering is done directly into the DOM (so that it can correctly
+         * compute positions). However, the views are always rendered in fragments,
+         * and appended to the DOM once ready (to prevent them from flickering). We
+         * here use the on_attach_callback hook, called when the widget is attached
+         * to the DOM, to perform the rendering. This ensures that the rendering is
+         * always done in the DOM.
+         *
+         * @override
+         */
+        on_attach_callback: function () {
+            this._super.apply(this, arguments);
+            this.isInDOM = true;
+            this._render();
+        },
+        /**
+         * @override
+         */
+        on_detach_callback: function () {
+            this._super.apply(this, arguments);
+            this.isInDOM = false;
+        },
         _render: function () {
-          // alert("renderRendered");
             var self = this;
             this.$el.empty();
             this.$el.append(qweb.render('ViewSmartTrafic', {
                 'groups': this.state,
             }));
+            
+            if(this.isInDOM){
+              this._renderMap();
+            }
+
             return this._super.apply(this, arguments);
         },
         _onClickButton: function (ev) {
@@ -37,6 +64,12 @@ odoo.define('Map.Renderer',function(require){
             this.trigger_up('btn_clicked', {
                 'domain': [['id', 'in', children_ids]]
             });
+        },
+        _renderMap: function(){
+          var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
+          var map = L.map('mapId').setView([23.140445, -82.351644], 17).addLayer(osm);
         }
     });
 
